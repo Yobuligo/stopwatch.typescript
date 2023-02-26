5b9dc76e131591cf3ce7c88358c41ecfb877ffaa
Yobuligo <hoffmannpeter82@gmail.com>
2023-02-26 11:11:20 +0100

    Add gitignore

--- node_modules/@yobuligo/core.typescript/dist/Lazy.d.ts
+++
@@ -1,9 +0,0 @@
-import { ILazy } from "./ILazy";
-export declare class Lazy<T> implements ILazy<T> {
-    private initializer;
-    private _isInitialized;
-    private _value?;
-    constructor(initializer: () => T);
-    get value(): T;
-    get isInitialized(): boolean;
-}
--- node_modules/@yobuligo/core.typescript/dist/Triple.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class Triple<A, B, C> {
-    readonly first: A;
-    readonly second: B;
-    readonly third: C;
-    constructor(first: A, second: B, third: C);
-}
