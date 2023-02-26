8875ce6163986f9600a8cc4e30ccf4bb0bba1b91
Yobuligo <hoffmannpeter82@gmail.com>
2023-02-26 16:16:36 +0100

    Recompile

--- dist/Stopwatch.js
+++ dist/Stopwatch.js
@@ -8,12 +8,12 @@ var StopwatchDefault = /** @class */ (function () {
     }
     Object.defineProperty(StopwatchDefault.prototype, "elapsed", {
         get: function () {
-            if (this.startTime !== undefined) {
-                if (this.endTime !== undefined) {
-                    return this.endTime.getTime() - this.startTime.getTime();
+            if (this._startedAt !== undefined) {
+                if (this._stoppedAt !== undefined) {
+                    return this._stoppedAt.getTime() - this._startedAt.getTime();
                 }
                 else {
-                    return new Date().getTime() - this.startTime.getTime();
+                    return new Date().getTime() - this._startedAt.getTime();
                 }
             }
             else {
@@ -39,31 +39,47 @@ var StopwatchDefault = /** @class */ (function () {
     });
     Object.defineProperty(StopwatchDefault.prototype, "isRunning", {
         get: function () {
-            return this.startTime !== undefined && this.endTime === undefined;
+            return this._startedAt !== undefined && this._stoppedAt === undefined;
         },
         enumerable: false,
         configurable: true
     });
     StopwatchDefault.prototype.reset = function () {
-        this.startTime = undefined;
-        this.endTime = undefined;
+        this._startedAt = undefined;
+        this._stoppedAt = undefined;
         this._intermediateTimes = [];
     };
     StopwatchDefault.prototype.start = function (text) {
         var _this = this;
         if (text === void 0) { text = "Start"; }
-        (0, core_typescript_1.ifNull)(this.startTime, function () {
-            _this.startTime = new Date();
-            _this.addIntermediateTime(text, _this.startTime);
+        (0, core_typescript_1.ifNull)(this._startedAt, function () {
+            _this._startedAt = new Date();
+            _this.addIntermediateTime(text, _this._startedAt);
         });
+        return this._startedAt;
     };
+    Object.defineProperty(StopwatchDefault.prototype, "startedAt", {
+        get: function () {
+            return this._startedAt;
+        },
+        enumerable: false,
+        configurable: true
+    });
     StopwatchDefault.prototype.stop = function (text) {
         if (text === void 0) { text = "Stop"; }
         if (this.isRunning) {
-            this.endTime = new Date();
-            this.addIntermediateTime(text, this.endTime);
+            this._stoppedAt = new Date();
+            this.addIntermediateTime(text, this._stoppedAt);
         }
+        return this._stoppedAt;
     };
+    Object.defineProperty(StopwatchDefault.prototype, "stoppedAt", {
+        get: function () {
+            return this._stoppedAt;
+        },
+        enumerable: false,
+        configurable: true
+    });
     StopwatchDefault.prototype.takeIntermediateTime = function (text) {
         if (text === void 0) { text = "Intermediate time"; }
         if (this.isRunning) {
---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Stopwatch = exports.createStopwatch = void 0;
+var Stopwatch_1 = require("./Stopwatch");
+Object.defineProperty(exports, "createStopwatch", { enumerable: true, get: function () { return Stopwatch_1.createStopwatch; } });
+Object.defineProperty(exports, "Stopwatch", { enumerable: true, get: function () { return Stopwatch_1.Stopwatch; } });
+//# sourceMappingURL=index.js.map
